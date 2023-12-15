c1c2bf963db83f15c4113a602a6669cc514ede45
Max <bruno.m.voss@gmail.com>
2023-12-15 18:36:25 +0100

    added more comments and explanation

--- panda_collisions/panda_collisions.py
+++ panda_collisions/panda_collisions.py
@@ -11,8 +11,68 @@ from panda3d.core import NodePath
 
 from panda_object_create import panda_object_create_load
 
+# Ok, crash course. This topic is a bit complicated, and may take multiple tries to get.
 
-# this one for help? https://discourse.panda3d.org/t/magical-collision-scene-optimizer/28978
+# highly recommend reading the manual if you're reading comments
+# https://docs.panda3d.org/1.10/python/programming/collision-detection/index
+# yes, do it again. Mind the version I'm linking here.
+
+# You want a collision between two objects.
+
+# fundamentally, the process goes like this:
+	
+	# ObjectA.setup()
+	# ObjectB.setup()
+	# collisiontraverser.traverse()
+	# your_collision = collisionhandlerqueue.getentries()
+	
+# Done.
+
+# Now for the details. E.g. "how to set up mouse click.
+
+# step 1
+# setup_mouse_click creates our "ObjectA" the ray that is being cast
+# from the camera, into the scene.
+
+# step 2
+# create_collision_node creates our "ObjectB" a sphere with radius 0.3
+
+# step 3
+# actually check for the collision with mouse_ray_check
+
+
+# Bonus step: Bitmasks 
+
+# pandas collision system is pretty clever.
+# some parts you WANT to check for collision, like [ray -> enemy] or [ray -> object]
+# and some parts you DONT want to check for collision, like [ray -> grass in front of enemy]
+
+# so panda has bit masks.
+# if two bitmasks OVERLAP, a collision CAN happen
+# if they don't, a collision CANT happen
+
+# e.g.  
+# "mouseray":"from":BitMask32(0b0111)
+# or 111
+# "item":"into":BitMask32(0b0100)
+# or 100
+
+# because the first bit overlaps, the mouse ray can collide with the item!
+
+# compared to:
+# "waypoint": "from":BitMask32(0b0000),
+# "NPC": "into":BitMask32(0b0011)},
+
+# because 000 and 011 DONT overlap, a collision can NEVER happen
+
+# ---
+
+# but because 
+# 111 from the mouse ray and "waypoint":"into":BitMask32(0b0100)
+# AND
+# 111 from the mouse ray and "NPC":"into":BitMask32(0b0011)},
+# BOTH overlap, but in different places,
+# the mouse ray will collide with both and you can click on both.
 
 class CollisionWrapper:
     def __init__(self,client=True):        
@@ -20,13 +80,14 @@ class CollisionWrapper:
         self.collision_objects = {}
         self.collision_bins = {}
         
+        # see the bitmask step at the beginning for what this means.
         self.bitmasks={"mouseray":
             {"from":BitMask32(0b0111),"into":BitMask32(0b0000)},
                         "terrain":
             {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
-                        "waypoint": # things that are already defined as waypoints
+                        "waypoint": 
             {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
-                        "item": # things that are not yet defined as waypoints, but are interactable.
+                        "item":
             {"from":BitMask32(0b0000),"into":BitMask32(0b0100)},
                         "players":
             {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
@@ -34,7 +95,6 @@ class CollisionWrapper:
             {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
                         "wall":
             {"from":BitMask32(0b0001),"into":BitMask32(0b0011)},
-            
             }
         
         
@@ -42,7 +102,7 @@ class CollisionWrapper:
         self.client_mouse_hover = client
         
         if client:
-            self.setup_mouse_click()
+            self.setup_mouse_ray()
         pusher = False
         if pusher:
         #alternative collisions handlers?
@@ -56,11 +116,15 @@ class CollisionWrapper:
         self.collision_bins[name]=P
     
     def update(self,input_d):
-        """this will require a dictionary as input"""
+        """
+        The input has to be structured as
+        
+        this will require a dictionary as input
+        
+        
+        """
         # tags I want... Actors, terrain, UI, Walls/structures?
         # then check the queue for the collisions I have found.
-        
-        
         
         if "create" in input_d:
             for ob_id in input_d["create"]:
@@ -117,9 +181,18 @@ class CollisionWrapper:
     
     def collision_checks(self):
         #actually look for collisions
+        
+        # node root is the root of a tree
+        # root
+        # - bin
+        #   - object1
+        #   - object2
+        # - bin 2
+        #   - object3
+        #   - object4
+        
         self.cTrav.traverse(self.node_root)
         collision_d = self.get_ids_from_collisions()
-        
         return collision_d
         
         if False:
@@ -128,32 +201,34 @@ class CollisionWrapper:
                     tag_tuple=("projectile",str(world_ob.id))
                     collision_mask=BitMask32.bit(1)
                     self.attach_collision_node(engine_ob,tag_tuple)
-        
-        
+         
     def create_collision_node(self,ob_id,tag_name):
         """
-        so the collision objects gets the same tag as the worldobject
+        ob_id and tagname are arbitray, but should be basic types that can be used as
+        dictionary keys.
+        
+        step 2 for the crash course
         """
         
-        # ok,so, terrain stuff is difficult because I'm not
-        # previously I was just using the terrian geometry.
-        
+        # same pattern as the mouse ray
         # define node type and shape
         col_Node = CollisionNode("general_col_node")
-        col_NodeNP=NodePath(col_Node)
-        
-        # this is for tracking
-        # node root because cTrav will iterate over it
-        col_NodeNP.reparentTo(self.node_root)
-        # tag is being used to recover the object
-        col_NodeNP.set_tag(tag_name,str(ob_id))
-        # object dict for updating and deleting.
-        self.collision_objects[ob_id]=col_NodeNP
-        
         col_body = CollisionSphere((0,0,0),0.3)
         col_Node.addSolid(col_body)
         
+        # track the object.
+        col_NodeNP = NodePath(col_Node)
+        col_NodeNP.reparentTo(self.node_root)
+        
+        # tag is being used to recover the object
+        col_NodeNP.set_tag(tag_name,str(ob_id))
+        
+        # add it to another datastructure to track it via id.
+        self.collision_objects[ob_id] = col_NodeNP
+        
         # set mask
+        # masks handle WHICH collisions are being looked for
+        # see __init__
         col_Node.setFromCollideMask(self.bitmasks[tag_name]["from"])
         col_Node.setIntoCollideMask(self.bitmasks[tag_name]["into"])
         
@@ -186,35 +261,41 @@ class CollisionWrapper:
         
         return output_d
     
-    def setup_mouse_click(self):
+    def setup_mouse_ray(self):
         """
-        this is the optional collision traverser for the client.
-        e.g. if stuff is being rendered, go through the iteration with
-        the ray and check when the mouse hovers on. the server doesn't need this however.
+        sets up the mouse ray?
+        
+        step 1 for the crash course
         """
-        #from the chess sample
-        self.picker = CollisionTraverser() 
-        self.CHQ_mouse = CollisionHandlerQueue()
+        
         # Make a collision node for our picker ray
         self.pickerNode = CollisionNode('mouseRay')
-        # Attach that node to the camera since the ray will need to be positioned
-        # relative to it
-        self.pickerNP = NodePath(self.pickerNode)
+        self.pickerRay = CollisionRay() 
+        self.pickerNode.addSolid(self.pickerRay)
         
-        # this is for tracking
-        # node root because cTrav will iterate over it
-        self.pickerNP.reparentTo(self.node_root)
-        
-        self.pickerNP = self.node_root.attachNewNode(self.pickerNode)
-        
+        # sets up WHICH collisions are being looked for, 
+        # see "bitmasks" in __init__
         self.pickerNode.setFromCollideMask(self.bitmasks["mouseray"]["from"])
         self.pickerNode.setIntoCollideMask(self.bitmasks["mouseray"]["into"])
+        
+        # this adds the ray to the "list" (not a list, but bear with me)
+        # of objects that are being checked for collisions called "node_root"
+        # "node_root" is the "input list"
+        self.pickerNP = NodePath(self.pickerNode)
+        self.pickerNP = self.node_root.attachNewNode(self.pickerNode) # not sure if I need this line.
+        self.pickerNP.reparentTo(self.node_root)
+        
+        
+        # this is the actual collision checker
+        self.MouseRayTraverser = CollisionTraverser()
+        
+        # this is the output list for lack of a better term.
+        self.CHQ_mouse = CollisionHandlerQueue()
+        
 
-        self.pickerRay = CollisionRay()  
-        # Add it to the collision node
-        self.pickerNode.addSolid(self.pickerRay)
-        # Register the ray as something that can cause collisions
-        self.picker.addCollider(self.pickerNP, self.CHQ_mouse)
+        # This means, "if there is a collision with my mouse ray, put it into the CHQ output list"
+        self.MouseRayTraverser.addCollider(self.pickerNP, self.CHQ_mouse)
+                
         
     def mouse_ray_check(self,mouse_data):
         """
@@ -222,13 +303,9 @@ class CollisionWrapper:
         
         passing around the lens object isn't ideal, but since this
         will only happen client side it should be fine.
+        
+        step 3 for the crash course
         """
-        
-        # highly recommend reading the manual if you're reading comments
-        # yes, do it again.
-        
-        mouse_collision_tags = {}
-        mouse_3d = None
         
         # Set the position of the ray based on the mouse position
         (Lens, X, Y), camera_position,camera_hpr = mouse_data
@@ -236,35 +313,56 @@ class CollisionWrapper:
         self.pickerNP.setHpr(camera_hpr)
         self.pickerRay.setFromLens(Lens, X, Y)
         
-        # this should be where the nodes are.
-        self.picker.traverse(self.node_root)
+        # self.node_root is your input list where all your objects
+        # are parented to.
+        self.MouseRayTraverser.traverse(self.node_root)
+        
+        # this is your output list
         self.CHQ_mouse.sortEntries()
+        n_entries = self.CHQ_mouse.getNumEntries() 
         
-        n_entries =self.CHQ_mouse.getNumEntries() 
         
+        # this whole last part answers the question: 
+        # "but WHICH object has my mouse ray collided with?"
         entry_index = 0
-        tag_names=["waypoint","terrain","object","NPC","wall","item"]
+        tag_names = ["waypoint","terrain","object","NPC","wall","item"]
         
+        mouse_collision_tags = {}
         for tag in tag_names:
             mouse_collision_tags[tag]=[]
             
+        mouse_3d = None
+        
+        # this is a bit awkward...
         while entry_index < n_entries and n_entries > 0:
-            collision_entry=self.CHQ_mouse.getEntry(entry_index)
-            collision_node=collision_entry.getIntoNode()
+			
+			# this is how we get the entry and collision object from the underlying C/C++ part of panda
+            collision_entry = self.CHQ_mouse.getEntry(entry_index)
+            collision_node = collision_entry.getIntoNode()
+            
+            # this gets the 3d coordinates of the collision, relative to the root node
             mouse_3d = collision_entry.getSurfacePoint(self.node_root)
+            
+            # tags are tuples that can be associated with objects
+            # as a "name", "data" pair
+            # so I'm using the "tag name" to get the id of the object
             for x in tag_names:
                 ob_id = collision_node.getTag(x)
-                if ob_id=='':
+                if ob_id == '':
                     continue
                 if ob_id not in mouse_collision_tags[x]:
-                    if ob_id not in mouse_collision_tags[x]:
-                        mouse_collision_tags[x].append(ob_id)
+					mouse_collision_tags[x].append(ob_id)
+			
+			# loop around.
             entry_index += 1
         
+        # clean up the dictionary.
         for tag in tag_names:
             if len(mouse_collision_tags[tag])==0:
                 mouse_collision_tags.pop(tag)
         
+        # return the last 3d collision point
+        # and the dict of all objects, grouped by tag, that the mouse ray has collided with.
         return mouse_3d, mouse_collision_tags
     
     def get_ids_from_collisions(self):
